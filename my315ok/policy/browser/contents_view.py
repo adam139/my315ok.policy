98765f1a25ce84754097806eb6e3f94377c6265b
adam139 <yuejun.tang@gmail.com>
2019-12-09 19:48:48 +0800

    add blog view

---
+++ my315ok/policy/browser/contents_view.py
@@ -0,0 +1,75 @@
+#-*- coding: UTF-8 -*-
+from plone import api
+from z3c.form import field
+import json
+from zope.event import notify
+from Acquisition import aq_inner
+from Acquisition import aq_parent
+from zope.interface import Interface
+from Products.Five.browser import BrowserView 
+from zope.component import getMultiAdapter
+from zope.component import queryUtility
+from Products.CMFCore.utils import getToolByName
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone.memoize.instance import memoize
+from zope.interface import Interface
+# from plone.directives import dexterity
+from plone.memoize.instance import memoize
+
+
+
+class BaseView(BrowserView):
+    "base view"  
+    
+    def __init__(self,context, request):
+        # Each view instance receives context and request as construction parameters
+        self.context = context
+        self.request = request
+#         add_resource_on_request(self.request, 'load-more')    
+    
+    @memoize    
+    def catalog(self):
+        context = aq_inner(self.context)
+        pc = getToolByName(context, "portal_catalog")
+        return pc
+    
+    @memoize    
+    def pm(self):
+        context = aq_inner(self.context)
+        pm = getToolByName(context, "portal_membership")
+        return pm    
+            
+    @property
+    def isEditable(self):
+        return self.pm().checkPermission(permissions.ManagePortal,self.context)
+       
+    def getobj_url(self,type):
+        catalog = api.portal.get_tool('portal_catalog')
+        brains = catalog(portal_type=type)
+        if bool(brains):
+            return brains[0].getURL()
+        else:
+            return ''
+    
+    def tranVoc(self,value):
+        """ translate vocabulary value to title"""
+        translation_service = getToolByName(self.context,'translation_service')
+
+        title = translation_service.translate(
+                                                  value,
+                                                  domain='plone',
+                                                  mapping={},
+                                                  target_language='zh_CN',
+                                                  context=self.context,
+                                                  default='')
+        return title     
+    
+class BlogView(BaseView):
+    "content type:blog view"
+
+    
+    def update(self):
+        # Hide the editable-object border
+        self.request.set('disable_border', True)
+        
+    
