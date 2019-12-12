c184a5c2bbb033b2b65ad5c5a45f7cb0f137fe72
adam139 <yuejun.tang@gmail.com>
2019-12-12 14:35:42 +0800

    query add order_by

--- my315ok/policy/migration.py
+++ my315ok/policy/migration.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 from my315ok.policy import engine
 from sqlalchemy import create_engine, Table, MetaData
-from sqlalchemy.sql import select       
+from sqlalchemy.sql import select, desc       
 from zope import event
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode
 from my315ok.policy.events import CreateDocEvent
+from my315ok.policy.contents.blog import IBlog
        
 def import_contents(context):
-    "import blogs"   
+    "import blogs"
+    
+    def IdIsExist(context,Id):
+        catalog = getToolByName(context, "portal_catalog")
+        brains = catalog(object_provides=IBlog.__identifier__,id=Id) 
+        return bool(brains)       
     
     def query(kwargs):
         """
@@ -25,13 +32,14 @@ def import_contents(context):
             meta = MetaData(engine)
             blogs = Table('plone_forum_post', meta, autoload=True)
             stm = select([blogs.c.pid, blogs.c.subject, blogs.c.message, blogs.c.dateline])\
-            .where(blogs.c.subject !=u"").limit(size).offset(offset)
+            .where(blogs.c.subject !=u"").order_by(desc(blogs.c.pid)).limit(size).offset(offset)
             rs = con.execute(stm)
         return rs.fetchall()
     
-    recorders = query({"size":2,"offset":103})
+    recorders = query({"size":6,"offset":0})
     for j in recorders:
         id = str(j[0])
+        if IdIsExist(context,id):continue
         title = safe_unicode(j[1])               
         text = safe_unicode(j[2])
         createdtime = j[3]               
